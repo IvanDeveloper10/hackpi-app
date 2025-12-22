3d1b1fe5779bf4236e24dad842108a60ea9f632d
IvanDeveloper10 <ivandeveloper99@gmail.com>
2025-12-22 17:47:21 -0500

    Problems Solved

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+import { nextui } from '@nextui-org/theme';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig; 
